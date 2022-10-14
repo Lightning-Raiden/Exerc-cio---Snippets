144059da5783185efe18147305814609cf3cff5d
-LightningRaiden- <109315626+Lightning-Raiden@users.noreply.github.com>
2022-10-13 21:38:20 -0300

    Add files via upload

---
+++ Exercício - Snippets/js/main.js
@@ -0,0 +1,7 @@
+var aposta = alert ('Escolha o seu animal para apostar:');
+
+if (aposta = macaco) {
+console.log ('Parabéns, você ganhou 70 mil! Para receber seu prêmio, mande para nós o número do seu cartão, os três numerozinhos de trás e seu cpf.');
+} else {
+console.log ('Não foi dessa vez, campeão. Boa sorte na próxima!');
+}
